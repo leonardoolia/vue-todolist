2d494ae72419c1b70ee16a662b317e95834ddda1
leonardoolia <leonardooliaa@gmail.com>
2024-01-23 14:29:47 +0100

    created data with array of objects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 const { createApp } = Vue;
 
-const app = createApp({});
+const app = createApp({
+    name: 'ToDo List',
+    data() {
+        return {
+            tasks: [
+                { id: 1, done: false, text: 'Fare la spesa' },
+                { id: 2, done: true, text: 'Lavare i capelli' },
+                { id: 3, done: false, text: 'Comprare una marca da bollo' },
+                { id: 4, done: false, text: 'Aggiornare il PC' }
+            ]
+        }
+    }
+
+});
 
 app.mount('#root')
